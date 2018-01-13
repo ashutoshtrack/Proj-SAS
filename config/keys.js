9e309ec728553ad6fccd53d0953129c1e63d97a4
ashutoshtrack <www.ashutoshtrack@gmail.com>
2018-01-13 16:19:42 +0530

    finished auth flow tweaked

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
   //keys
-  module.exports = require("./keys");
+  module.exports = require("./prod");
 } else {
   //dev
   module.exports = require("./dev");
