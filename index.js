cc3640eef0ce2a93995d305f3313f8ad8303bf99
ashutoshtrack <www.ashutoshtrack@gmail.com>
2018-01-16 15:42:14 +0530

    done2

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(paspport.session());
 require("./routes/authRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   const path = require("path");
 
