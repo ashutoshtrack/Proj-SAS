ab05776f04496a032915a4092481e8f5cac89cc8
XenonAman <amanmahato888@gmail.com>
2018-03-04 23:28:32 +0530

    Carousel and Dropdown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,37 +1,83 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 //import { Link } from "react-router-dom";
-import { Navbar, NavItem, Nav } from "react-bootstrap";
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
+var FontAwesome = require("react-fontawesome");
 
 class Header extends Component {
   renderContent() {
+    const Droptitle = (
+      <span class="gearSize">
+        <FontAwesome name="cogs" />
+      </span>
+    );
     switch (this.props.auth) {
       case null:
         return (
-          <Nav pullRight>
-            <NavItem eventKey={5}>Processing</NavItem>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={5}>Processing</NavItem>
+            </Nav>
+          </Navbar.Collapse>
         );
 
       case false:
         return (
-          <Nav pullRight>
-            <NavItem eventKey={1}>About Us</NavItem>
-            <NavItem eventKey={2}>Stats</NavItem>
-            <NavItem eventKey={3} href="#abc">
-              Get In
-            </NavItem>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1}>About Us</NavItem>
+              <NavItem eventKey={2}>Stats</NavItem>
+              <NavItem eventKey={3} href="#abc">
+                Get In
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         );
 
       default:
         return (
-          <Nav pullRight>
-            <NavItem eventKey={3} href="/api/logout">
-              Logout
-            </NavItem>
-            <NavItem eventKey={4}>Hi, {this.props.auth.userName}</NavItem>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem>
+                <img
+                  src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
+                  class="navPic"
+                />&nbsp;&nbsp;
+                {this.props.auth.userName}
+              </NavItem>
+              <NavDropdown
+                eventKey={3}
+                title={Droptitle}
+                id="basic-nav-dropdown"
+              >
+                <MenuItem eventKey={3.1}>
+                  Edit Profile
+                  <span class="iconSize">
+                    <FontAwesome name="edit" />
+                  </span>
+                </MenuItem>
+                <MenuItem eventKey={3.2}>
+                  View Profile
+                  <span class="iconSize">
+                    <FontAwesome name="eye" />
+                  </span>
+                </MenuItem>
+                <MenuItem eventKey={3.3}>
+                  Save JobList
+                  <span class="iconSize">
+                    <FontAwesome name="bookmark" />
+                  </span>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3} href="/api/logout">
+                  Logout
+                  <span class="iconSize">
+                    <FontAwesome name="sign-out" />
+                  </span>
+                </MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
           /* <ul className="right">
             <li>
               <a href="/api/logout">Logout</a>
@@ -49,11 +95,12 @@ class Header extends Component {
     console.log(this.props);
 
     return (
-      <Navbar>
+      <Navbar inverse collapseOnSelect class="navDeco">
         <Navbar.Header>
           <Navbar.Brand>
             <a href="/new">Stud-Start</a>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
         {this.renderContent()}
       </Navbar>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,93 +1,144 @@
 import React, { Component } from "react";
 import { Animated } from "react-animated-css";
+import { Carousel } from "react-bootstrap";
 //import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
 
 class Landing extends Component {
   render() {
     return (
-      <Animated
-        animationIn="bounceInUp"
-        animationOut="fadeOut"
-        isVisible={true}
-      >
-        <div className="row" id="abc">
-          <Animated
-            animationIn="zoomInRight"
-            animationOut="fadeOut"
-            isVisible={true}
-          >
-            <div className="head_title text-center">
-              <h1 style={{ color: "gold" }}>Select Your Role!</h1>
-              <div className="separator">
-                <div className="boxs" />
+      <div>
+        <Animated
+          animationIn="bounceInUp"
+          animationOut="fadeOut"
+          isVisible={true}
+        >
+          <div className="row">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  width={900}
+                  height={500}
+                  alt="900x500"
+                  src="https://react-bootstrap.github.io/carousel.png"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  width={900}
+                  height={500}
+                  alt="900x500"
+                  src="https://react-bootstrap.github.io/carousel.png"
+                />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  width={900}
+                  height={500}
+                  alt="900x500"
+                  src="https://react-bootstrap.github.io/carousel.png"
+                />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl
+                    consectetur.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+
+          <div className="row" id="abc">
+            <Animated
+              animationIn="zoomInRight"
+              animationOut="fadeOut"
+              isVisible={true}
+            >
+              <div className="head_title text-center">
+                <h1 style={{ color: "gold" }}>Select Your Role!</h1>
+                <div className="separator">
+                  <div className="boxs" />
+                </div>
+              </div>
+            </Animated>
+
+            <div className="col-md-4 col-sm-4">
+              <div className="eventimg">
+                <h2>Students</h2>
+                <br />
+                <p>
+                  "Random Text for dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum."
+                </p>
+                <br />
+                <a href="/student" className="btn">
+                  Register Here!
+                </a>
               </div>
             </div>
-          </Animated>
 
-          <div className="col-md-4 col-sm-4">
-            <div className="eventimg">
-              <h2>Students</h2>
-              <br />
-              <p>
-                "Random Text for dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum."
-              </p>
-              <br />
-              <a href="/student" className="btn">
-                Register Here!
-              </a>
+            <div className="col-md-4 col-sm-4">
+              <div className="eventimg center">
+                <h2>Startups</h2>
+                <br />
+                <p>
+                  "Random Text for dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum."
+                </p>
+                <br />
+                <a href="www.google.com" className="btn">
+                  Register Here!
+                </a>
+              </div>
+            </div>
+
+            <div className="col-md-4 col-sm-4">
+              <div className="eventimg center">
+                <h2>Angels</h2>
+                <br />
+                <p>
+                  "Random Text for dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                  aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum."
+                </p>
+                <br />
+                <a href="www.google.com" className="btn">
+                  Register Here!
+                </a>
+              </div>
             </div>
           </div>
-
-          <div className="col-md-4 col-sm-4">
-            <div className="eventimg center">
-              <h2>Startups</h2>
-              <br />
-              <p>
-                "Random Text for dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum."
-              </p>
-              <br />
-              <a href="www.google.com" className="btn">
-                Register Here!
-              </a>
-            </div>
-          </div>
-
-          <div className="col-md-4 col-sm-4">
-            <div className="eventimg center">
-              <h2>Angels</h2>
-              <br />
-              <p>
-                "Random Text for dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum."
-              </p>
-              <br />
-              <a href="www.google.com" className="btn">
-                Register Here!
-              </a>
-            </div>
-          </div>
-        </div>
-      </Animated>
+        </Animated>
+      </div>
       //end of main div
     );
   }
